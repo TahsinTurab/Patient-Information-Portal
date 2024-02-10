007e10674c60a10fc11a965b9664b17786a9751e
Tahsin <tahsinhasanturab@gmail.com>
2024-02-10 21:19:41 +0600

    pushed

--- PatientInformationAPI.Tests/obj/Debug/net6.0/PatientInformationAPI.Tests.AssemblyInfo.cs
+++ PatientInformationAPI.Tests/obj/Debug/net6.0/PatientInformationAPI.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PatientInformationAPI.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+badc38770ba5d1f71129ca1941355c9860a2b1cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PatientInformationAPI.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PatientInformationAPI.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
